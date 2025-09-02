a4e76444e9e53f4b2c29725e536a5af5c5c4abe1
Elfish Akoj's <akojaelijah450@gmail.com>
2025-09-02 10:46:13 -0700

    feat: added dice roll logic and the reset button

--- script.js
+++ script.js
@@ -23,14 +23,25 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+//Starting conditions
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player0El.classList.remove('player-winner');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player-active');
+};
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -62,7 +73,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // Check if the player score is >= 100
-    if (scores[activePlayer] > 20) {
+    if (scores[activePlayer] > 100) {
       //finish the gane
       playing = false;
       diceEl.classList.add('hidden');
@@ -79,3 +90,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//Resetting the game
+
+btnNew.addEventListener('click', init);
